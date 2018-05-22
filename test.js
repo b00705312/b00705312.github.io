cb8f3f5e3b0e431f41f97008d483ae693b9e18a8
Ferid <b00705312@essec.edu>
2018-05-22 15:24:07 +0200

    2

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-setTimeout(function(){
-    document.querySelector("h1").style.color="#ff0000"
-}, 3000);
+setTimeout(
+    setInterval(function(){
+    document.querySelector("h1").style.color="green"
+}, 3000),3000);
