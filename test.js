57474e47acb9424b15f058ae07fa563ef644722e
Ferid <b00705312@essec.edu>
2018-05-23 11:39:57 +0200

    today

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
-setTimeout(
-    setInterval(function(){
-    document.querySelector("h1").style.color="green"
-}, 3000),5000);
 
 function clickonh1(){
     alert("You clicked on me");
+    document.querySelection("h1");
 }
